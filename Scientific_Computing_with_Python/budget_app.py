7157383ee5b3b7853e9ae86a71ef7f37ea3dd711
foalic <foalic-undulation@posteo>
2021-03-28 14:41:20 +0100

    - Begun coding the create_spend_chart function and subfunctions to keep it clean.

--- Scientific_Computing_with_Python/budget_app.py
+++ Scientific_Computing_with_Python/budget_app.py
@@ -1,4 +1,5 @@
 class Category:
+    withdrawn_amount_total = 0
 
     def __init__(self, name):
         self.name = name
@@ -55,6 +56,7 @@ class Category:
         if self.check_funds(amount):
             self.ledger.append({"amount": -amount, "description": description})
             self.balance -= amount
+            self.withdrawn_amount_total += amount
             return True
         return False
 
@@ -79,8 +81,70 @@ class Category:
 
 
 def create_spend_chart(categories):
-    
-    pass
+    name_lists, full_amount_spent = get_category_namelists_and_amounts_spent(categories)
+    rounded_percentage_dict = get_rounded_percentage_of_spent(categories, full_amount_spent)
+    header = "Percentage spent by category\n"
+    body_list = make_body_list()
+
+    bar_separator = "---" * len(categories) + "-"
+
+    prepped_name_lists = make_vertical_category_names(name_lists)
+    chart = prepped_name_lists, full_amount_spent, rounded_percentage_dict, body_list
+    return chart
+
+
+def get_category_namelists_and_amounts_spent(categories):
+    name_lists = []
+    full_amount_spent = 0
+
+    for category in categories:
+        name_lists.append(list(category.name))
+        full_amount_spent += category.withdrawn_amount_total
+
+    return name_lists, full_amount_spent
+
+
+def get_rounded_percentage_of_spent(categories, full_amount_spent):
+    percentage_of_full_amount_dict = {}
+    rounded_percentages_dict = {}
+
+    for category in categories:
+        percentage_of_full_amount_dict.update({category.name:((category.withdrawn_amount_total * 100)/full_amount_spent)})
+        unrounded_percentage = percentage_of_full_amount_dict.get(category.name)
+        rounded_percentage = (unrounded_percentage // 10) * 10
+        rounded_percentages_dict.update({category.name:rounded_percentage})
+
+    return rounded_percentages_dict
+
+
+def make_body_list():
+    body_list = []
+    for number in range(11):
+        number_setup = " " + str(number) + "0| "
+        if number == 0:
+            number_setup = "  0| "
+        elif number == 10:
+            number_setup = str(number) + "0| "
+        body_list.append(number_setup)
+    body_list.reverse()
+    return body_list
+
+
+def make_vertical_category_names(name_lists):
+    longest_list_length = 0
+    for list in name_lists:
+        if len(list) > longest_list_length:
+            longest_list_length = len(list)
+    # return longest_list_length
+
+    for list in name_lists:
+        if len(list) < longest_list_length:
+            for extra_char in range((longest_list_length - len(list))):
+                list.append(" ")
+    #return name_lists
+
+    ## Need to code a list which combines corresponding indices from each list together
+
 
 
 food = Category("food")
@@ -88,6 +152,7 @@ clothes = Category("clothes")
 food.deposit(200.50, "Aubergine")
 food.transfer(100, clothes)
 
+clothes.withdraw(33.24, "petrol")
 print(food.ledger)
 print(food.balance)
 
@@ -95,3 +160,4 @@ print(clothes.ledger)
 print(clothes.get_balance())
 
 print(food)
+print(create_spend_chart([food, clothes]))
