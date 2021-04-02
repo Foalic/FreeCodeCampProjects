670f2ccf2d552c7d7f7d7b473cdeec949af73b66
foalic <foalic-undulation@posteo>
2021-04-02 17:28:52 +0100

    - Added experiment function, using a subfunction to get how many times the expected combination of balls appears, and pass that number to the experiment function to calculate the probability

--- Scientific_Computing_with_Python/probability_calculator.py
+++ Scientific_Computing_with_Python/probability_calculator.py
@@ -10,14 +10,15 @@ class Hat:
             for count in range(value):
                 self.contents.append(ball_colour)
 
-    def draw(self, number_of_balls):
+
+    def draw(self, num_balls_drawn):
         drawn_balls = []
         balls_left = [ball for ball in self.contents]
 
-        if number_of_balls > len(self.contents):
+        if num_balls_drawn > len(self.contents):
             drawn_balls = self.contents
         else:
-            for draw in range(number_of_balls):
+            for draw in range(num_balls_drawn):
                 rand_ball = random.randint(0, (len(balls_left)-1))
                 drawn_balls.append(balls_left[rand_ball])
                 balls_left.pop(rand_ball)
@@ -25,11 +26,50 @@ class Hat:
         return drawn_balls
 
 
+
+def get_number_of_matched_experiments(hat, expected_balls, num_balls_drawn):
+    matched_experiments = 0
+    drawn_balls = hat.draw(num_balls_drawn)
+    drawn_balls_dict = dict()
+    is_match = False
+
+    for ball in drawn_balls:
+        if ball not in drawn_balls_dict:
+            drawn_balls_dict[ball] = 1
+        else:
+            drawn_balls_dict[ball] += 1
+
+    for key_drawn,value_drawn in drawn_balls_dict.items():
+        for key_expected,value_expected in expected_balls.items():
+            if key_expected == key_drawn:
+                if value_drawn >= value_expected:
+                    is_match = True
+                    break
+                else:
+                    continue
+        else:
+            continue
+
+        if is_match:
+            matched_experiments += 1
+
+    return matched_experiments
+
+
+
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    balls_in_hat = [ball for ball in hat.contents]
 
+    for experiment in range(num_experiments):
+        matched_experiments= get_number_of_matched_experiments(hat, expected_balls, num_balls_drawn)
 
-hat = Hat(red=2, blue=5, green=3)
-print(hat.contents)
-print(hat.draw(4))
-print(hat.contents)
+    probability = matched_experiments / num_experiments
+    return probability
+
+#
+# hat = Hat(red=9, blue=10, green=30)
+# print(hat.contents)
+# print(hat.draw(4))
+# print(hat.contents)
+#
+# print(experiment(hat, {"red":1, "blue":1, "green":1}, 20, 2000))
