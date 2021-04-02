0bc9048499643cebf861551617dd96ca77eff30a
foalic <foalic-undulation@posteo>
2021-04-02 13:24:00 +0100

    - Removed unnecessary error codes.

--- Scientific_Computing_with_Python/probability_calculator.py
+++ Scientific_Computing_with_Python/probability_calculator.py
@@ -6,11 +6,7 @@ class Hat:
 
     def __init__(self, **kwargs):
         self.contents = []
-        if len(kwargs) == 0:
-            raise ValueError("Missing arguments: please enter at least one ball colour and an amount above 1 in format 'red=1'.")
         for ball_colour,value in kwargs.items():
-            if value == 0 and len(kwargs) == 1:
-                raise ValueError("Missing arguments: please enter at least one ball colour and an amount above 1 in format 'red=1'.")
             for count in range(value):
                 self.contents.append(ball_colour)
 
