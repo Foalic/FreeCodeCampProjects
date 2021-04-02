9a767278e972a2dfe3a137a98c0f128f6e31f7f7
foalic <foalic-undulation@posteo>
2021-04-02 13:39:10 +0100

    - Added draw function

--- Scientific_Computing_with_Python/probability_calculator.py
+++ Scientific_Computing_with_Python/probability_calculator.py
@@ -10,9 +10,26 @@ class Hat:
             for count in range(value):
                 self.contents.append(ball_colour)
 
+    def draw(self, number_of_balls):
+        drawn_balls = []
+        balls_left = [ball for ball in self.contents]
+
+        if number_of_balls > len(self.contents):
+            drawn_balls = self.contents
+        else:
+            for draw in range(number_of_balls):
+                rand_ball = random.randint(0, (len(balls_left)-1))
+                drawn_balls.append(balls_left[rand_ball])
+                balls_left.pop(rand_ball)
+
+        return drawn_balls
+
+
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
 
 
-hat = Hat(red=0)
+hat = Hat(red=2, blue=5, green=3)
+print(hat.contents)
+print(hat.draw(4))
 print(hat.contents)
